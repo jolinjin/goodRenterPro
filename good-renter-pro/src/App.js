f8b3ea6aaf98e25efc20a334f4695ecb3ba67c8c
Yihe Jin <jinyihe666@gmail.com>
2024-06-03 22:21:00 +0800

    init router

--- good-renter-pro/src/App.js
+++ good-renter-pro/src/App.js
@@ -1,10 +1,32 @@
 import React from 'react'
-import {Button} from 'antd-mobile'
+import { Button } from 'antd-mobile'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+import CityList from './pages/CityLits/index'
+import Home from './pages/Home/index'
 
 export default function App() {
   return (
-    <div>
-      <Button color='primary'>click</Button>
-    </div>
+    <Router>
+      <div className='App'>
+        {/* 项目根组件 <button>登录</button> */}
+
+        {/* 配置导航菜单 */}
+        <ul>
+          <li>
+            <Link to="/home">首页</Link>
+
+          </li>
+          <li>
+            <Link to="/Citylist">城市选择</Link>
+
+          </li>
+
+        </ul>
+        {/* 配置路由 */}
+        <Route path="/home" component={Home}></Route>
+        <Route path="/Citylist" component={CityList}></Route>
+      </div>
+    </Router>
   )
 }
