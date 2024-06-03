b5fb9853b505b670a8b05a13f642b573c6baa23c
Yihe Jin <jinyihe666@gmail.com>
2024-06-03 15:00:58 +0800

    init project

---
+++ good-renter-pro/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Button} from 'antd-mobile'
+
+export default function App() {
+  return (
+    <div>
+      <Button color='primary'>click</Button>
+    </div>
+  )
+}
